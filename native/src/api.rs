316f0957bbc9c0b6763a59f5901ae5d5ade55f07
Koning van de Maan <smoky@Konings-MacBook-Pro.local>
2023-06-28 18:32:29 -0700

    clean rust template

--- native/src/api.rs
+++ native/src/api.rs
@@ -11,20 +11,6 @@ pub enum Platform {
 }
 
 pub fn platform() -> Platform {
-    // This is a macro, a special expression that expands into code. In Rust, all macros
-    // end with an exclamation mark and can be invoked with all kinds of brackets (parentheses,
-    // brackets and curly braces). However, certain conventions exist, for example the
-    // vector macro is almost always invoked as vec![..].
-    //
-    // The cfg!() macro returns a boolean value based on the current compiler configuration.
-    // When attached to expressions (#[cfg(..)] form), they show or hide the expression at compile time.
-    // Here, however, they evaluate to runtime values, which may or may not be optimized out
-    // by the compiler. A variety of configurations are demonstrated here which cover most of
-    // the modern oeprating systems. Try running the Flutter application on different machines
-    // and see if it matches your expected OS.
-    //
-    // Furthermore, in Rust, the last expression in a function is the return value and does
-    // not have the trailing semicolon. This entire if-else chain forms a single expression.
     if cfg!(windows) {
         Platform::Windows
     } else if cfg!(target_os = "android") {
@@ -44,8 +30,6 @@ pub fn platform() -> Platform {
     }
 }
 
-// The convention for Rust identifiers is the snake_case,
-// and they are automatically converted to camelCase on the Dart side.
 pub fn rust_release_mode() -> bool {
     cfg!(not(debug_assertions))
 }
